25579d5a6a67d818fcb25b497e947e2bd247d766
Fiona McLaren <fionamclaren268@gmail.com>
2024-02-24 16:25:07 +0000

    added some structure to our project. the src file will have all our screens and any custom components we use stored there. some pages have been made already. navigation structure added too and now start point goes to end point page. next steps: fiona will work on getting user data transferring between the start point to the end point. hopefully newer structure means people can work on the other pages easily

---
+++ src/screens/Walk.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Text, View} from "react-native";
+
+export default function Routes() {
+
+   	const styles = {
+   		container: "flex flex-1 items-center justify-center",
+   	};
+
+   	return (
+            <View className={styles.container}>
+               <Text>Walk page</Text>
+            </View >
+    );
+
+}
+
