71ae453bb5bae5694a6892cac3aceda94b0eb291
matthewduffy33 <duffy.matt33@gmail.com>
2024-02-24 23:02:37 +0000

    Basic implementation of a map

--- src/screens/StartPoint.js
+++ src/screens/StartPoint.js
@@ -1,20 +1,25 @@
 import React, {useState} from "react";
 import {Text, View, Button} from "react-native";
+import MapPicker from "../Map/MapLocationPicker";
 
 export default function StartPoint({ navigation }) {
+    const [start, setStart] = useState("");
 
    	const styles = {
-   		container: "flex flex-1 items-center justify-center",
+   		container: "items-center justify-center h-4/5 bg-neutral-950",
    	};
 
 
    	return (
-            <View className={styles.container}>
+
+            <View className={styles.container }>
+               <MapPicker start={start} changeStart={setStart}/>
                <Text>Start Point</Text>
                   <Button
                     title="Select end point"
                     onPress={() => navigation.navigate("End Point")}
                   />
+               <Text>{start}</Text>
             </View >
     );
 
