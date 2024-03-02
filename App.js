3ee5866f5b08a567a2713e014e6a17834f538a8d
matthewduffy33 <duffy.matt33@gmail.com>
2024-03-02 22:34:59 +0000

    a completely broken version of getting the location

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import StartPoint from './src/screens/StartPoint';
 import EndPoint from './src/screens/EndPoint';
 import Routes from './src/screens/Routes';
 import Walk from './src/screens/Walk';
+import StartWalk from './src/screens/StartWalk';
 
 import { Provider as PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
@@ -16,6 +17,7 @@ export default function App() {
               <Stack.Navigator initialRouteName="Starting Point">
                 <Stack.Screen name="Starting Point" component={StartPoint} />
                 <Stack.Screen name="End Point" component={EndPoint} />
+                <Stack.Screen name="StartWalk" component={StartWalk} />
                 <Stack.Screen name="Walk" component={Walk} />
                 <Stack.Screen name="Routes" component={Routes} />
               </Stack.Navigator>
--- src/screens/Walk.js
+++ src/screens/Walk.js
@@ -1,15 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {Text, View} from "react-native";
+import {WalkMap} from '../components/Map/WalkMap.js';
+import {Location} from '../components/Routes/Location.js';
+import Geolocation from '@react-native-community/geolocation';
+import {Polyline} from "../components/Routes/PolylineRequest";
+import {decode} from "@googlemaps/polyline-codec";
 
-export default function Routes() {
+export default function Walk({route, navigation}) {
 
    	const styles = {
-   		container: "flex flex-1 items-center justify-center",
+   		container: "flex flex-1 justify-center",
    	};
+
+
+
+
+
+	const [currLoc, setLoc] = useState(route.params.startingLoc);
+	const polyline = new Polyline("Route ", decode("{_wsIro}XdIoCqGmL"), [], 0, "0s");
+
+	useEffect(() => {
+		console.log(currLoc);
+	});
+
+	Geolocation.watchPosition(
+		loc => {
+			setLoc(new Location("De la Sol (Me, Myself and I)", loc["coords"]["latitude"], loc["coords"]["longitude"]));
+
+		},
+		error => {
+			console.log(error.code, error.message);
+		},
+		{distanceFilter: 100},
+	);
 
    	return (
             <View className={styles.container}>
                <Text>Walk page</Text>
+				<WalkMap current={currLoc} polyline={polyline}/>
             </View >
     );
 
