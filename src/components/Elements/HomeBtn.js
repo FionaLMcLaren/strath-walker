b88bd0f0a1b4a575a813306e17aa014047cd18bf
Fiona McLaren <fionamclaren268@gmail.com>
2024-03-09 12:16:08 +0000

    nativewind v4 update and added fonts - although not rendering for me yet. home page buttons done

---
+++ src/components/Elements/HomeBtn.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { View, Pressable } from "react-native";
+import {Icon, Text } from "react-native-paper";
+import classNames from 'classnames';
+
+export default function HomeBtn({title, icon, colour, action}) {
+
+    return (
+        <Pressable
+            onPress={action}
+            className={classNames(
+                " rounded-md border-2 border-b-8 border-r-8 p-4 w-80 h-24 flex justify-center overflow-hidden" +
+                " active:border-b-2 active:border-r-2 active:scale-95 transition-all duration-200 ",
+                colour==="tq" && "bg-teal-400",
+                colour==="pk" && "bg-pink-300",
+                colour==="yl" && "bg-yellow-300")}>
+
+
+            <View className="flex ">
+                <View className="absolute scale-125 -translate-x-5 -translate-y-5 bg-white border-2 p-4">
+                    <Icon
+                        source={icon}
+                        size={40}
+                        className="text-white stroke-black"
+                    />
+                </View>
+
+                <View className="flex flex-row gap-1 self-end ">
+                    <Text
+                        className="text-xl translate-y-0.5 antialiased tracking-wide font-extrabold "
+                    >
+                        {title}
+                    </Text>
+
+                    <Icon
+                        source="arrow-right"
+                        size={30}
+                        className="text-white stroke-black"
+                    />
+                </View>
+            </View>
+
+        </Pressable>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {
-      'yellow': '#fdd756',
-      'turquoise': '#51bab4',
-      'pink': '#f5b5c3',
+      'tq': '#51bab4',
+      'yl': '#fdd756',
+      'pk': '#f5b5c3',
       'white': '#fff',
       'light-grey': '#f0f0f0',
       'dark-grey': '262525',
       'black': '#000',
     },
+
   },
   plugins: [],
 }
