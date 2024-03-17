542db98e097c5aa17cf6ef42eff4b42fbba7e8c8
Emma Thorpe <emma.thorpe.2021@uni.strath.ac.uk>
2024-03-17 11:10:46 +0000

    pace values

--- src/screens/WalkDataView.js
+++ src/screens/WalkDataView.js
@@ -4,6 +4,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import {Polyline} from "react-native-maps";
 import {ResultMap} from "../components/Map/ResultMap";
 import {PrevWalkMap} from "../components/Map/PrevWalkMap";
+import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
 
 export default function WalkDataView({navigation}) {
 
@@ -20,29 +21,58 @@ export default function WalkDataView({navigation}) {
   }
 
   useEffect( () => {
-    void getWalkData();
+    void getWalkData()
   }, []);
 
+  useEffect(() => {
+    calcAvgPace()
+  }, [walkData])
+
   const [selectedWalk, setSelectedWalk] = useState(null);
+  const [avgPace, setAvgPace] = useState(null);
+
+  const calcAvgPace = () => {
+    console.log("averaging");
+
+    let totalPace = 0;
+    let counter = 0;
+
+    console.log(walkData);
+
+    walkData.forEach(nextWalkData => {
+      if (nextWalkData.pace !== undefined) {
+        totalPace += nextWalkData.pace;
+        counter++;
+      }
+    });
+
+    if (counter > 0) {
+      const averagePace = totalPace / counter;
+      setAvgPace(averagePace);
+    }
+  }
 
   if(walkData.length > 0){
     return (
       <View className={styles.container}>
         <PrevWalkMap walk={selectedWalk} />
         <ScrollView>
+            {
+              avgPace
+                ?   <Text>Average pace: {avgPace}m/s</Text>
+                :   <Text>Average pace: N/A</Text>
+            }
+
           {
-            walkData
-              ?   walkData.map((nextWalkData, index) => {
+            walkData.map((nextWalkData, index) => {
                   return (
                     <WalkOption
                                 key={index}
                                 data={nextWalkData}
                                 onPress={() => {
                                   setSelectedWalk(nextWalkData);
-                                }
-                                }/>)
+                                }}/>)
               })
-              :   <Text>Loading...</Text>
           }
         </ScrollView>
 
@@ -65,6 +95,7 @@ const WalkOption=({ data, onPress })=> {
         <Text>{data['selectedRoute'][0]['name']} to {data['selectedRoute'][data['selectedRoute'].length - 1]['name']}</Text>
         <Text>Walked from {new Date(data['startTime']).toLocaleTimeString("en-GB", {timeStyle:"short"})} to {new Date(data['endTime']).toLocaleTimeString("en-GB", {timeStyle:"short"})} on {new Date(data['startTime']).toLocaleDateString("en-GB", {dateStyle:"short"})}</Text>
         <Text>{data['distance']}m in {data['steps']} steps</Text>
+        <Text>{data['pace']}m/s</Text>
       </View>
     </TouchableHighlight>
   );
