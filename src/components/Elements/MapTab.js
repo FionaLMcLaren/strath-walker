cce9fdd3e14abcfd7063ea2c8230991ff9eb8e85
Fiona McLaren <fionamclaren268@gmail.com>
2024-03-18 23:04:03 +0000

    adding bugsnag to hopefully find out error with the apk crashes and more button cleanup

--- src/components/Elements/MapTab.js
+++ src/components/Elements/MapTab.js
@@ -9,7 +9,7 @@ export default function MapTab(props) {
                 "absolute bottom-0 z-30 bg-teal-400 w-full rounded-md border-2 border-t-4",
                 props.routePage && "h-[28rem]",
                 props.walkPage && "h-[25rem]",
-                props.endPage && "h-[36rem]"
+                props.endPage && "h-[35rem]"
                 )}></View>
             <View className="absolute bottom-0 z-40 bg-white p-4 rounded-md mx-2 border-2 -translate-y-2 mb-2 ">
                 {props.children}
