8a1e1653d807760babc17e0fca0db285c8c6e411
matthewduffy33 <duffy.matt33@gmail.com>
2024-03-18 09:14:40 +0000

    fixed errors with end walk renderer and the walk page

--- src/components/Walking/EndWalkRenderer.js
+++ src/components/Walking/EndWalkRenderer.js
@@ -3,16 +3,18 @@
 //Animates a recreation of a walk by just waiting a second and adding another coordinate to the line till done
 export function renderEndWalk(coord, pushNewLine, changePos){
 
+    if(coord.length !== 0){
+        let animateCoord = setInterval(function(){
+            let newNode = coord.shift()
 
-    let animateCoord = setInterval(function(){
-        let newNode = coord.shift()
+            if(coord.length === 0){
+                clearInterval(animateCoord);
+            }
 
-        if(coord.length === 0){
-            clearInterval(animateCoord);
-        }
+            pushNewLine(newNode);
 
-        pushNewLine(newNode);
+            changePos(newNode);
+        }, 500);
+    }
 
-        changePos(newNode);
-    }, 500);
 }
