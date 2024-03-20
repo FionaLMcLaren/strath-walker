fabb89c08aba321cee3aa197db5334f7eedc887d
Fiona McLaren <fionamclaren268@gmail.com>
2024-03-20 23:30:41 +0000

    removed bugsnag performance plugin as wasnt working and causing errors for others

--- index.js
+++ index.js
@@ -5,7 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import BugsnagPerformance from '@bugsnag/react-native-performance'
 
-BugsnagPerformance.start('')
 AppRegistry.registerComponent(appName, () => App);
